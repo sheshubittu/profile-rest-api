e2db321043f5af73decc19e205f309712e9cd646
Shashank <shashankpetkar007@gmail.com>
2023-04-30 01:17:47 +0530

    Added custome user profile,manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profiles_api',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
